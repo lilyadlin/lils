1244e248989940cec5d36458ad87c0cfe1e10731
lilyadlin <75860543+lilyadlin@users.noreply.github.com>
2025-06-04 15:08:12 -0700

    Updated website: removed index.html references and simplified social media links

---
+++ firstproj.jsx
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Stunning Website</title>
+</head>
+<body>
+ <h1>Lily Adlin Title</h1>
+ <h2>Portfolio of Personability</h2>
+    <div id="topNav"></div>
+    <div id="main"></div>
+    <div id="footer"></div>
+
+</body>
+</html>
